3d38c2b918836af9a3a51ce1ba33e804921968ed
Prachi <raj.prachi290@gmail>
2022-02-18 08:31:06 +0530

    LinkedList

---
+++ LinkedList.java
@@ -0,0 +1,34 @@
+class LinkedList {
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    public void print() {
+        Node n = head;
+        while (n != null) {
+            System.out.println(n.data);
+            n = n.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        Node fourth = new Node(4);
+        list.head.next = second;
+        second.next = third;
+        third.next = fourth;
+        list.print();
+
+    }
+}
